6eaf606f32861b95a9a8f72d5652b6db53806951
hao.long <hoolongvfx@hotmail.com>
2019-12-17 01:48:59 +0800

    Fix wrong command line.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
-# /user/bin/bash
-docker-compose up -d
-docker-compose exec svn svnadmin create --fs-type fsfs pipeline
-docker-compose exec svn htpasswd -b /etc/subversion/passwd svn-admin svn-admin
-
+#!/usr/bin/bash
+docker-compose up -d # Setup docker.
+docker-compose exec svn svnadmin create --fs-type fsfs pipeline # Create SVN repository.
+docker-compose exec svn htpasswd -b /etc/subversion/passwd svn-admin svn-admin # Create Admin user.
