4b2dfc58898bc8e7e51678bb348fd788f4449987
hao.long <hoolongvfx@hotmail.com>
2019-12-17 01:34:50 +0800

    fix typo

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
-# /bin/bash
-svnadmin create --fs-type fsfs pipeline
-htpasswd -b /etc/subversion/passwd svn-admin svn-admin
+# /user/bin/bash
+docker-compose up -d
+docker-compose exec svn svnadmin create --fs-type fsfs pipeline
+docker-compose exec svn htpasswd -b /etc/subversion/passwd svn-admin svn-admin
+
